db6b95f53e92829883eebe740017f3e64b54a83f
SupperGabor <gabor1415@gmail.com>
2024-05-14 13:38:26 +0200

    fixed typo

--- app.js
+++ app.js
@@ -3,19 +3,18 @@
 //For each number starting from 1 up to 135:
 //Display the number.
 
-for(let i = 1; i <= 135; i++) {
-    console.log(`Number: ${i}`)
-};
-
+for (let i = 1; i <= 135; i++) {
+  console.log(`Number: ${i}`);
+}
 
 //Print odd numbers from 1 to 135:
 
 //For each number starting from 1 up to 135:
-//If the number is odd:
+//Increment numbers starting from 1 by 2,
 //Show the number.
 
-for(let j = 1; j <= 135; j+=2) {
-    console.log(`Odd numbers: ${j}`)
+for (let j = 1; j <= 135; j += 2) {
+  console.log(`Odd numbers: ${j}`);
 }
 
 //3 Print numbers and their sum from 1 to 135:
@@ -27,9 +26,9 @@ for(let j = 1; j <= 135; j+=2) {
 
 let sum = 0;
 
-for(let i = 1; i <= 135; i++) {
-    sum += i;
-    console.log(`Number: ${i}, Sum: ${sum}`)
+for (let i = 1; i <= 135; i++) {
+  sum += i;
+  console.log(`Number: ${i}, Sum: ${sum}`);
 }
 
 //4 Print all numbers from an array:
@@ -37,10 +36,10 @@ for(let i = 1; i <= 135; i++) {
 //For each number in the array:
 //Show the number.
 
-let x = [1,4,2,12]
+let x = [1, 4, 2, 12];
 
-for(let i = 0; i < x.length; i ++) {
-    console.log(x[i])
+for (let i = 0; i < x.length; i++) {
+  console.log(x[i]);
 }
 
 //5 Find the maximum number in an array:
@@ -51,19 +50,16 @@ for(let i = 0; i < x.length; i ++) {
 //Set maxNumber to the current number.
 //Display the maxNumber.
 
-
-const y = [2,-3,-1, 5]
-let maxNumber = 0
-
+const y = [2, -3, -1, 5];
+let maxNumber = 0;
 
 for (let i = 0; i < y.length; i++) {
-    if(maxNumber < y[i]) {
-        maxNumber = y[i]
-    }
+  if (maxNumber < y[i]) {
+    maxNumber = y[i];
+  }
 }
 
-
-console.log(maxNumber)
+console.log(maxNumber);
 
 //6. Calculate the average of numbers in an array:
 
@@ -73,17 +69,17 @@ console.log(maxNumber)
 //Calculate the average by dividing the sum by the total number of elements in the array.
 //Display the average.
 
-const z = [2,-3,-1, 5]
+const z = [2, -3, -1, 5];
 
 let average = 0;
 let numbersAdded = 0;
 
-for(let i = 0; i < z.length; i++) {
-    numbersAdded += z[i];   
+for (let i = 0; i < z.length; i++) {
+  numbersAdded += z[i];
 }
 
-average = numbersAdded / z.length
-console.log(average)
+average = numbersAdded / z.length;
+console.log(average);
 
 //7 Replace negative numbers with 0 in an array:
 
@@ -92,14 +88,14 @@ console.log(average)
 //Change the number to 0.
 //Display the modified array.
 
-const withNegatives = [2,-1,4,-3]
+const withNegatives = [2, -1, 4, -3];
 
-for (let i = 0 ; i < withNegatives.length; i++) {
-    if(withNegatives[i] < 0) {
-        withNegatives[i] = 0
-    }
+for (let i = 0; i < withNegatives.length; i++) {
+  if (withNegatives[i] < 0) {
+    withNegatives[i] = 0;
+  }
 }
-console.log(withNegatives)
+console.log(withNegatives);
 
 //8 Replace negative numbers with a string in an array:
 
@@ -108,11 +104,11 @@ console.log(withNegatives)
 //Change the number to "Turing".
 //Display the modified array.
 
-const arrayNegatives = [2,-1,4,-3]
+const arrayNegatives = [2, -1, 4, -3];
 
-for (let i = 0 ; i < arrayNegatives.length; i++) {
-    if(arrayNegatives[i] < 0) {
-        arrayNegatives[i] = "Turing"
-    }
+for (let i = 0; i < arrayNegatives.length; i++) {
+  if (arrayNegatives[i] < 0) {
+    arrayNegatives[i] = "Turing";
+  }
 }
-console.log(arrayNegatives)
+console.log(arrayNegatives);
